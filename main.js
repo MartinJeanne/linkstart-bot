2473302767c6a675e31aa89bb765addbaed3ef01
Martin Jeanne <mjeanne@sio.local>
2020-12-07 15:26:37 +0100

    addbing some command to give cookie !

--- main.js
+++ main.js
@@ -10,24 +10,53 @@ client.once("ready", ()=>{
 client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(' ');
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    switch(command) {
-        case "ping" :
-            message.channel.send('pong !');
-            break;
+    if (command === 'cookie') {
 
-        case "cookie" :
-            message.channel.send('Cookie');
-            break
+        if (args[0] === 'give') {
 
-        case 'args-info' :
+            if (!message.mentions.users.size) {
+                return message.reply('you need to tag a user in order to give a cookie to them !');
+            }
+
+            if (args[1] != null) {
+                const amount = parseInt(args[1]);
+            
+                if (isNaN(amount)) {
+                    return message.reply('that doesn\'t seem to be a valid number.');
+                }
+
+                else if (amount < 2 || amount > 100) {
+                    message.reply('the number of cookie given must be between 2 and 100 !')
+                }
+
+                else {
+                    message.channel.send(message.mentions.users.first().username + ' you received ' + amount + ' cookie from ' + message.author.username + ' !')
+                }
+            }
+            else {
+                message.channel.send(message.mentions.users.first().username + ' you received a cookie from ' + message.author.username + ' !')
+            }
+
+        }
+        else {
+            message.channel.send('i give you a cookie ' + message.author.username);
+        }
+    }
+
+
+
+    else if (command === 'ping') {
+        message.channel.send('pong !');
+    }
+    else if (command === 'args-info') {
             if (!args.length) {
                 return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
             }
             message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-    }
+        }
 })
 
 client.login(token)
