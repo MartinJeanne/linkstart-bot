6614ee7c8927b5f2b00fa4906c32057f174773f3
Martin Jeanne <mjeanne@sio.local>
2020-12-07 17:08:54 +0100

    refractoring the code so that it is more understanble !

--- main.js
+++ main.js
@@ -21,12 +21,14 @@ client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
 
-    if (!client.commands.has(command)) return;
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
 
     try {
-        client.commands.get(command).execute(message, args);
+        command.execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply('There was an error trying to execute that command !');
