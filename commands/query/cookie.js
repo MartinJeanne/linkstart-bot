d9c7af99b9757de465a2a859d8108d96d9d31467
Garwalle <martin.jeanne@sts-sio-caen.info>
2020-12-17 17:14:28 +0100

    Changing the ceateConnection to connect the remote database !

--- commands/query/cookie.js
+++ commands/query/cookie.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var bdd = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'discord'
+    host: 'eu-cdbr-west-03.cleardb.net',
+    user: 'bde9cf4f063788',
+    password: 'b891c331',
+    database: 'heroku_584b0ee0edf98c7'
 });
 
 bdd.connect(function (err) {
