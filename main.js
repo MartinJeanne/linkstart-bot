7bd4a0263be7cc8f8578b5a108406838b10e20cc
Garwalle <martin.jeanne@sts-sio-caen.info>
2020-12-17 16:33:06 +0100

    deploy 5

--- main.js
+++ main.js
@@ -101,5 +101,5 @@ client.on('message', message => {
     }
 })
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN); // BOT_TOKEN is the Client Secret
 
