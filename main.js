3474fc5c0474d1ac7c8a5a96a5d0c5996d16d746
Garwalle <martin.jeanne@sts-sio-caen.info>
2020-12-19 20:58:20 +0100

    Irrevelant, testing title and description
    This should be my commit description !

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection(); // To add cooldown on certain command
 
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
