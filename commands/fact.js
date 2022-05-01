556aaa27ac78bb2a4e32b44fdacbe490560fabd0
MartinJeanne <martinjeanne.dev@gmail.com>
2022-05-01 12:21:15 +0200

    fact command can take two parameter

--- commands/fact.js
+++ commands/fact.js
@@ -5,24 +5,30 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('fact')
 		.setDescription('Tell a Chuck Norris fact!')
-		.addStringOption(option => option.setName('fullname')
-				.setDescription('Enter your firstname and lastname, separated by a space')
-				.setRequired(false)),
+		.addStringOption(option => option.setName('firstname')
+			.setDescription('Put your firstname in the fact')
+			.setRequired(false))
+		.addStringOption(option => option.setName('lastname')
+			.setDescription('Put your lastname in the fact')
+			.setRequired(false)),
+
 	async execute(interaction) {
-		let name = interaction.options.getString('fullname');
-		let requestArg = ''
-		if (name) {
-			let arr = name.split(' ');
-			if (arr.length == 2) requestArg = `?firstName=${arr[0]}&lastName=${arr[1]}`
-			else return await interaction.reply({ content: 'The fullname you provided is incorrect, it must me like a firstname followed by a lastname with a space between. Example: "Chuck Norris".', ephemeral: true });
+		let firstname = interaction.options.getString('firstname');
+		let lastname = interaction.options.getString('lastname');
+
+		let requestArg = '';
+		if (firstname) {
+			requestArg = '?firstName=' + firstname;
+			if (lastname) requestArg =+ '&lastName=' + lastname;
 		}
-		
-		let myjoke;
+		else if (lastname) requestArg = '?firstName=Chuck&lastName=' + lastname;
+
+		let data;
 		await axios.get('http://api.icndb.com/jokes/random' + requestArg)
 			.then(response => {
-				myjoke = response.data;
+				data = response.data;
 			})
 			.catch(error => console.log(error));
-		await interaction.reply(myjoke.value.joke);
+		await interaction.reply(data.value.joke);
 	},
 };
