9096c76e539ae7e712aebf90d8e07ae8296f25b3
MartinJeanne <martinjeanne.dev@gmail.com>
2022-05-02 23:39:27 +0200

    test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('./ressources/config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -35,4 +35,4 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
-client.login(token);
+client.login(process.env.TOKEN2); // Change from locale to remote
