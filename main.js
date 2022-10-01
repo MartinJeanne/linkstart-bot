8a258a35ad3790c89e641c3b7109260689f5c8e1
MartinJeanne <martinjeanne.dev@gmail.com>
2022-10-01 20:45:38 +0200

    Adding guild_member intent

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
+const music = require('@koenie06/discord.js-music');
 //const { token } = require('./ressources/config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
