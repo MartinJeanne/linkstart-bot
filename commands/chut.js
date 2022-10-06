cdd5b15845df6c6a73c4487d0f0cff9e5e5ca8c7
MartinJeanne <martin.jeanne@sinay.fr>
2022-10-06 11:31:35 +0200

    Improving chut command again, some message are ephemeral, translating command description to french, getting the bot id dynamically

--- commands/chut.js
+++ commands/chut.js
@@ -1,37 +1,38 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('chut')
-		.setDescription('Fait comprendre à quelqu\'un qui doit la fermer')
-		.addUserOption(option => option.setName('user')
-			.setDescription('The user that need to shut his mouth')
+		.setDescription("Je dis à quelqu'un de la fermer, il ne saura pas que c'est toi qui à lancé la commande !")
+		.addUserOption(option => option.setName('membre')
+			.setDescription('La personne qui doit se taire')
 			.setRequired(true)),
 
-	async execute(interaction) {
-		mentionnedUser = interaction.options.getUser('user');
+	async execute(interaction, client) {
+		mentionnedUser = interaction.options.getUser('membre');
 		const chutSentence = [
-			`TA GUEULE ${mentionnedUser}`, 
-			`Ferme ton clapet ${mentionnedUser}.`, 
-			`Tais-toi ${mentionnedUser} !`, 
-			`Ferme ta boîte à camembert ${mentionnedUser}.`, 
+			`TA GUEULE ${mentionnedUser}`,
+			`Ferme ton clapet ${mentionnedUser}.`,
+			`Tais-toi ${mentionnedUser} !`,
+			`Ferme ta boîte à camembert ${mentionnedUser}.`,
 			`${mentionnedUser}, je crois qu'il faut que tu te taises.`
 		];
 
 		if (mentionnedUser === interaction.member.user.id) {
-			await interaction.reply("Heu, t'es bizarre fréritot..");
+			await interaction.reply({ content: "Heu, t'es bizarre fréritot..", ephemeral: true });
 		}
 
-		else if (mentionnedUser == '784536536459771925') {
-			await interaction.reply("Tu t'es pris pour qui ? J'vais te goumer.");
+		else if (mentionnedUser == client.user.id) {
+			await interaction.reply({ content: "Tu t'es pris pour qui ? J'vais te goumer.", ephemeral: true });
 		}
 
 		else if (mentionnedUser == '306129521990565888') {
-			await interaction.reply("Es-tu sûr de vouloir faire ça ?");
+			await interaction.reply({ content: "Es-tu sûr de vouloir faire ça ?", ephemeral: true });
 		}
 
 		else {
-			await interaction.reply({ content: `Je m'en occupe.`, ephemeral: true});
+			await interaction.reply({ content: `Je m'en occupe.`, ephemeral: true });
 			const random = Math.floor(Math.random() * chutSentence.length);
 			await interaction.channel.send(chutSentence[random]);
 		}
