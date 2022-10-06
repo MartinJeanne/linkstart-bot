7d0fd15a9baeb37a9c1620704f71d886ab30be04
MartinJeanne <martinjeanne.dev@gmail.com>
2022-10-07 00:00:42 +0200

    Redeploying command to a single discord server, it seem faster

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./ressources/configTest.json');
+const { REST, Routes } = require('discord.js');
+const { clientId, token, guildId } = require('./ressources/config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -13,6 +12,6 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationCommands(clientId), { body: commands })
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Les commandes ont été correctement enregistrées !'))
 	.catch(console.error);
