8b3afdd652c4e4d9c01ebc40a2b4df90133951e7
MartinJeanne <martin.jeanne@sinay.fr>
2022-12-28 10:21:22 +0100

    Adding command: 'pause'

--- commands/pause.js
+++ commands/pause.js
@@ -1,10 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('file')
-		.setDescription('Affiche la file des musiques'),
+		.setName('pause')
+		.setDescription('Pause ou relance la musique'),
 
 	async execute(interaction, client) {
 		await interaction.deferReply({ ephemeral: true }); // make Discord API wait for reply
@@ -17,21 +16,16 @@ module.exports = {
 		else if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId)
 			return await interaction.editReply('Tu dois être dans le même salon vocal que moi pour exécuter cette commande !');
 
-		// TODO tracks or nowPlaying
+
 		const queue = client.player.getQueue(interaction.guildId);
 		if (!queue || !queue.playing) return await interaction.editReply('Je ne joue pas de musique actuellement !');
-
-		let embed = new EmbedBuilder()
-			.setColor(0x6df4d0)
-			.setTitle('File des musiques')
-			.addFields({ name: 'Actuelle', value: queue.nowPlaying().toString() })
-			.setTimestamp()
-			.setFooter({ text: 'Bonne écoute !', iconURL: 'https://cdn.discordapp.com/avatars/784536536459771925/03a8dc68b874f740def806a36675633e.webp?size=128' });
-
-		for (let i = 0; i < queue.tracks.length; i++) {
-			embed.addFields({ name: `${i + 1}.`, value: queue.tracks[i].toString() })
+		if (queue.setPaused()) {
+			queue.setPaused(false)
+			return await interaction.editReply('Reprise de la musique');
 		}
-
-		return await interaction.editReply({ embeds: [embed] });
+		else {
+			queue.setPaused(true);
+			return await interaction.editReply('Pause');
+		}
 	},
 };
