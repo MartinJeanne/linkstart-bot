fda1097fa4d0de908a360ed526e45b2cb0156a45
MartinJeanne <martin.jeanne@sinay.fr>
2022-12-30 17:30:26 +0100

    Adding command: "mélange"

---
+++ src/commands/music/mélange.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+const checkPlayerUsable = require('../../functions/checkPlayerUsable.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('mélange')
+        .setDescription('Mélange les musiques'),
+
+    async execute(interaction, client) {
+        await interaction.deferReply(); // make Discord API wait for reply
+
+        const queue = await checkPlayerUsable(interaction, client);
+        if (!queue) return;
+
+        queue.shuffle();
+        await interaction.editReply(`:twisted_rightwards_arrows: File mélangé !`);
+    },
+};
