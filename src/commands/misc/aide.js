ccb3451d94d0d521f13711387d9629f1e6c16d08
MartinJeanne <martinjeanne.dev@gmail.com>
2022-12-31 15:54:28 +0100

    Improving display of commands in /aide

--- src/commands/misc/aide.js
+++ src/commands/misc/aide.js
@@ -16,7 +16,7 @@ module.exports = {
 			const commandFiles = fs.readdirSync(`src/commands/${folder}`).filter(file => file.endsWith('.js'));
 			for (const file of commandFiles) {
 				const command = require(`../${folder}/${file}`);
-				commands.push(command.data);
+				commands.push({ fodler: folder, data: command.data });
 			}
 		}
 
@@ -26,11 +26,17 @@ module.exports = {
 			.setTimestamp()
 			.setFooter({ text: `Merci !`, iconURL: 'https://cdn.discordapp.com/avatars/784536536459771925/03a8dc68b874f740def806a36675633e.webp?size=128' });
 
+		let folderName = '';
 		for (let i = 0; i < commands.length; i++) {
-			embed.addFields({ name: `${commands[i].name}`, value: `${commands[i].description}` });
+			if (commands[i].fodler != folderName) {
+				folderName = commands[i].fodler;
+				let title = folderName;
+				title = title.charAt(0).toUpperCase() + title.slice(1); // First letter uppercase
+				embed.addFields({ name: '\u200B', value: `__**${title}**__` });
+			}
+			embed.addFields({ name: commands[i].data.name, value: commands[i].data.description });
 		}
 
-		console.log(commands[0]);
 		await interaction.editReply({ embeds: [embed] });
 	},
 };
