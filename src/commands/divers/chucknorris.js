8070564e12117ecc3f72ceed61f2c7c333511073
MartinJeanne <martin.jeanne@sinay.fr>
2023-01-02 11:36:05 +0100

    Renaming chuck norris facts command

--- src/commands/divers/chucknorris.js
+++ src/commands/divers/chucknorris.js
@@ -3,16 +3,15 @@ const { default: axios } = require('axios');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('fact')
-		.setDescription('Donne une "Chuck Norris fact"'),
+		.setName('chucknorris')
+		.setDescription('Envoie une des "Chuck Norris facts"'),
 
 	async execute(interaction) {
 		let joke;
 		await axios.get('https://api.chucknorris.io/jokes/random')
-			.then(response => {
-				joke = response.data.value;
-			})
+			.then(response => joke = response.data.value)
 			.catch(error => console.log(error));
+
 		await interaction.editReply(joke);
 	},
 };
