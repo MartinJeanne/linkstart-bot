b5a6c774bf0f57cdeb69429d8f2b519c3a7622a8
MartinJeanne <martin.jeanne@sinay.fr>
2023-01-02 14:27:51 +0100

    making registerEvents async and reorganise it

--- src/functions/registerEvents.js
+++ src/functions/registerEvents.js
@@ -1,23 +1,4 @@
-module.exports = function (client) {
-    // Once bot is started
-    client.once('ready', () => {
-        console.log(`${client.user.tag} est lancé !`)
-    });
-
-    // When member join the server
-    client.on('guildMemberAdd', member => {
-        // Adding "Nouveau" to new user when they join the server
-        if (member.guild.id == '485000880114892821') member.roles.add('485021407529664526');
-    });
-
-    // When member leave the server
-    client.on('guildMemberRemove', member => {
-        if (member.guild.id == '485000880114892821') {
-            const channel = member.guild.channels.cache.find(ch => ch.name === 'chat-modérateur');
-            channel.send(`Bye, ${member}`);
-        }
-    });
-
+module.exports = async function (client) {
     // When user uses a slash (/) command!
     client.on('interactionCreate', async interaction => {
         if (!interaction.isCommand()) return;
@@ -33,5 +14,24 @@ module.exports = function (client) {
             console.error(error);
             await interaction.editReply({ content: "❌ Une erreur c'est produite lors de l'exécution de cette commande, reportez ce problème à un modérateur", ephemeral: true });
         }
+    });
+    
+    // When member join the server
+    client.on('guildMemberAdd', member => {
+        // Adding "Nouveau" to new user when they join the server
+        if (member.guild.id == '485000880114892821') member.roles.add('485021407529664526');
+    });
+
+    // When member leave the server
+    client.on('guildMemberRemove', member => {
+        if (member.guild.id == '485000880114892821') {
+            const channel = member.guild.channels.cache.find(ch => ch.name === 'chat-modérateur');
+            channel.send(`Bye, ${member}`);
+        }
+    });
+
+    // Once bot is started
+    client.once('ready', () => {
+        console.log(`${client.user.tag} est lancé !`)
     });
 };
