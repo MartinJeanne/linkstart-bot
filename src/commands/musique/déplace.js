9c690eed592605e1416e6c1e73d0a4393ea9c48a
MartinJeanne <martin.jeanne@sinay.fr>
2023-01-03 16:44:04 +0100

    Draft of /déplace

---
+++ src/commands/musique/déplace.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+const checkPlayerUsable = require('../../functions/checkPlayerUsable.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('déplace')
+        .setDescription('Déplace une musique à une certaine position'),
+
+    // TODO
+    async execute(interaction, client) {
+        const queue = await checkPlayerUsable(interaction, client);
+        if (!queue) return;
+
+        if (queue.tracks.length < 2) return await interaction.editReply(':interrobang: Il doit y avoir au moins deux musiques dans la file pour créer une playlist');
+
+        await interaction.editReply('Playlist crée avec comme nom : TODO');
+    },
+};
