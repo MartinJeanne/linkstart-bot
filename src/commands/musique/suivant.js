b7ecc5565d992154bfa2141cb3752f951f5e8832
MartinJeanne <martin.jeanne@sinay.fr>
2023-01-03 16:43:53 +0100

    Improve /suivant response + lines at the end of files

--- src/commands/musique/suivant.js
+++ src/commands/musique/suivant.js
@@ -10,7 +10,9 @@ module.exports = {
 		const queue = await checkPlayerUsable(interaction, client);
 		if (!queue) return;
 
+		const skipped = queue.nowPlaying();
+		const current = queue.tracks[0];
 		queue.skip();
-		return await interaction.editReply('⏩ Musique passé');
+		return await interaction.editReply(`⏩ Musique passé : **${skipped.title}**\nMusique actuelle : **${current.title}**`);
 	},
 };
