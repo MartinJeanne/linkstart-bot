487ccb5a15a7641c467c0234a5c385ed85d67662
MartinJeanne <martin.jeanne@sinay.fr>
2023-01-03 14:32:18 +0100

    /file button now show if usefull Externalized some /file code in queueMessageBuilder.js

--- src/commands/musique/file.js
+++ src/commands/musique/file.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
-const createQueueEmbed = require('../../functions/createQueueEmbed.js');
+const { SlashCommandBuilder, ComponentType } = require('discord.js');
+const { queueEmbedBuilder, queueRowBuilder } = require('../../functions/queueMessageBuilder.js');
 const checkPlayerUsable = require('../../functions/checkPlayerUsable.js');
 
 module.exports = {
@@ -11,40 +11,29 @@ module.exports = {
 		const queue = await checkPlayerUsable(interaction, client);
 		if (!queue) return;
 
-		// TODO improve button style
-		const row = new ActionRowBuilder()
-			.addComponents(
-				new ButtonBuilder()
-					.setCustomId('left')
-					.setLabel('⬅️')
-					.setStyle(ButtonStyle.Primary),
-
-				new ButtonBuilder()
-					.setCustomId('right')
-					.setLabel('➡️')
-					.setStyle(ButtonStyle.Primary),
-			);
-
 		let page = 0;
-		const embed = await createQueueEmbed(queue, page);
-		const message = await interaction.editReply({ embeds: [embed], components: queue.tracks.length > 10 ? [row] : [] });
+		const embed = await queueEmbedBuilder(queue, page);
+		const row = await queueRowBuilder(queue, page);
+		const message = await interaction.editReply({ embeds: [embed], components: row ? [row] : [] });
 
 		const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60000 });
 		collector.on('collect', async interaction => {
 
 			switch (interaction.customId) {
-				case 'right':
-					if (page >= Math.ceil(queue.tracks.length / 10) - 1) break;
-					page += 1;
-					break;
 				case 'left':
 					if (page <= 0) break;
 					page -= 1;
 					break;
+
+				case 'right':
+					if (page >= Math.ceil(queue.tracks.length / 10) - 1) break;
+					page += 1;
+					break;
 			}
 
-			const embed = await createQueueEmbed(queue, page);
-			await interaction.update({ embeds: [embed] });
+			const embed = await queueEmbedBuilder(queue, page);
+			const row = await queueRowBuilder(queue, page);
+			await interaction.update({ embeds: [embed], components: row ? [row] : [] });
 		});
 	},
 };
---
+++ src/functions/queueMessageBuilder.js
@@ -0,0 +1,42 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+exports.queueEmbedBuilder = async function (queue, page) {
+    const progress = queue.getPlayerTimestamp();
+
+    let queueString = '';
+    for (let i = (page * 10); i < (page * 10 + 10); i++) {
+        if (i >= queue.tracks.length) break;
+        queueString += `**${i + 1}.** ${queue.tracks[i].title}\n`;
+    }
+
+    return new EmbedBuilder()
+        .setColor(0xd7667e)
+        .setTitle(`${queue.nowPlaying().title}`)
+        .setDescription(`${progress.current} : ${progress.end}\n\n` + queueString)
+        .setThumbnail(queue.nowPlaying().thumbnail)
+        .setTimestamp()
+        .setFooter({
+            text: `\nPage : ${page + 1}/${Math.ceil(queue.tracks.length / 10)}`,
+            iconURL: 'https://cdn.discordapp.com/avatars/784536536459771925/03a8dc68b874f740def806a36675633e.webp?size=128'
+        });
+};
+
+
+exports.queueRowBuilder = async function (queue, page) {
+    if (queue.tracks.length < 10) return null;
+
+    function pageButtonBuilder(id, emoji) {
+        return new ButtonBuilder()
+            .setCustomId(id)
+            .setLabel(emoji)
+            .setStyle(ButtonStyle.Primary);
+    }
+
+    const leftBtn = page <= 0 ? null : pageButtonBuilder('left', '⬅️');
+    const rigthBtn = page >= Math.ceil(queue.tracks.length / 10) - 1 ? null : pageButtonBuilder('right', '➡️');
+
+    if (leftBtn && rigthBtn) return new ActionRowBuilder().addComponents(leftBtn ? leftBtn : null, rigthBtn);
+    else if (rigthBtn) return new ActionRowBuilder().addComponents(rigthBtn);
+    else if (leftBtn) return new ActionRowBuilder().addComponents(leftBtn);
+}
+
