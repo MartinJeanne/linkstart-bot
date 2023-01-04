dcb2dbf024b41b7de0738634e1e7d08e007ced99
MartinJeanne <martin.jeanne@sinay.fr>
2023-01-04 14:30:11 +0100

    Adding info to implement later in /playlist

--- src/commands/musique/playlist.js
+++ src/commands/musique/playlist.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { Track, Playlist } = require('discord-player');
 const checkPlayerUsable = require('../../functions/checkPlayerUsable.js');
 
 module.exports = {
@@ -6,7 +7,6 @@ module.exports = {
         .setName('playlist')
         .setDescription('Joue ou sauvegarde une playlist !'),
 
-    // TODO
     async execute(interaction, client) {
         const queue = await checkPlayerUsable(interaction, client);
         if (!queue) return;
@@ -15,5 +15,32 @@ module.exports = {
 
         //await interaction.editReply('Playlist crée avec comme nom : TODO');
         await interaction.editReply('Commande pas encore implémenté');
+
+        // TODO save my own tracks en playlists in my database
+        const track = new Track(client.player, {
+            title: string,
+            description: string,
+            author: string,
+            url: string,
+            thumbnail: string,
+            duration: string,
+            views: number,
+            requestedBy: User,
+        });
+
+        const playlist = new Playlist(client.player, {
+            tracks: [Track],
+            title: string,
+            description: string,
+            thumbnail: string,
+            type: "album" | "playlist",
+            source: TrackSource,
+            author: {
+                name: string,
+                url: string,
+            },
+            id: string,
+            url: string,
+        });
     },
 };
