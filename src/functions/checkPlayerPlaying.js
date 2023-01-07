55b7ce00b1dd88a0ce3ccc9269b58d4fb7228cf4
MartinJeanne <martinjeanne.dev@gmail.com>
2023-01-07 20:37:51 +0100

    When getting the queue, checking if interaction was replied

--- src/functions/checkPlayerPlaying.js
+++ src/functions/checkPlayerPlaying.js
@@ -1,11 +1,17 @@
 /** Check if user can use Player commands */
 module.exports = async function (interaction, client) {
 	// if user is not in channel
-	if (!interaction.member.voice.channel)
-		return await interaction.editReply(':interrobang: Tu dois être dans un salon vocal pour exécuter cette commande !');
+	if (!interaction.member.voice.channel) {
+		if (!interaction.replied) await interaction.editReply(':interrobang: Tu dois être dans un salon vocal pour exécuter cette commande !');
+		else await interaction.followUp(':interrobang: Tu dois être dans un salon vocal pour exécuter cette commande !');
+		return null;
+	}
 	// if i'm in channel AND user is not in my channel
-	else if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId)
-		return await interaction.editReply(':interrobang: Tu dois être dans le même salon vocal que moi pour exécuter cette commande !');
+	else if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) {
+		if (!interaction.replied) await interaction.editReply(':interrobang: Tu dois être dans le même salon vocal que moi pour exécuter cette commande !');
+		else await interaction.followUp(':interrobang: Tu dois être dans le même salon vocal que moi pour exécuter cette commande !');
+		return null;
+	}
 
 	const queue = client.player.getQueue(interaction.guildId);
 	if (!queue || !queue.playing) {
