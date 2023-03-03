22f72e4f2350169d1882ec432bdcf8ea83e86fee
MartinJeanne <martin.jeanne@sinay.fr>
2023-03-03 15:32:07 +0100

    /file v6 + refactor

--- src/commands/musique/file.js
+++ src/commands/musique/file.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, ComponentType } = require('discord.js');
-const { queueEmbedBuilder, queueRowBuilder } = require('../../functions/queueMessageBuilder.js');
+const { queueEmbedBuilder, queueRowBuilder } = require('../../functions/queueMsgBuilder.js');
 const getQueue = require('../../functions/getQueue.js');
 
 module.exports = {
@@ -12,8 +12,8 @@ module.exports = {
 		if (!queue) return;
 
 		let page = 0;
-		const embed = await queueEmbedBuilder(queue, page);
-		const row = await queueRowBuilder(queue, page);
+		const embed = queueEmbedBuilder(queue, page);
+		const row = queueRowBuilder(queue, page);
 		const message = await interaction.editReply({ embeds: [embed], components: row ? [row] : [] });
 
 		const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 120000 });
@@ -26,13 +26,13 @@ module.exports = {
 					break;
 
 				case 'right':
-					if (page >= Math.ceil(queue.tracks.length / 10) - 1) break;
+					if (page >= Math.ceil(queue.getSize() / 10) - 1) break;
 					page += 1;
 					break;
 			}
 
-			const embed = await queueEmbedBuilder(queue, page);
-			const row = await queueRowBuilder(queue, page);
+			const embed = queueEmbedBuilder(queue, page);
+			const row = queueRowBuilder(queue, page);
 			await inter.update({ embeds: [embed], components: row ? [row] : [] });
 		});
 
