fcd98a4a49506ec6daba8a113ad001d6e785b428
Garwalle <Martin.Jeanne@sts-sio-caen.info>
2023-05-30 15:15:37 +0200

    improve /roll

--- src/commands/divers/roll.js
+++ src/commands/divers/roll.js
@@ -4,15 +4,35 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roll')
 		.setDescription("Jette un dé !")
-		.addIntegerOption(option => option.setName('valeur')
+		.addStringOption(option => 
+			option.setName('action')
+			.setDescription('Laction à réaliser')
+			.setRequired(false))
+		.addIntegerOption(option => 
+			option.setName('nombre-face')
 			.setDescription('La valeur max du dé')
+			.setRequired(false))
+		.addIntegerOption(option => 
+			option.setName('modificateur')
+			.setDescription('Bonus ou malus')
 			.setRequired(false)),
 
 	async execute(interaction, client) {
-		const parameter = interaction.options.getInteger('valeur');
+		const action = interaction.options.getString('action');
+		const parameter = interaction.options.getInteger('nb face');
+		const modifier = interaction.options.getInteger('modificateur');
 		const maxValue = parameter != null ? parameter : 20;
 
 		const result = Math.floor(Math.random() * maxValue) + 1;
-		await interaction.editReply({ content: `🎲 **${result}**/${maxValue}`});
+		let response = `🎲 **${result}**/${maxValue}`;
+		if (modifier) {
+			const modifierSigne = modifier > 0 ? '+' : '';
+			response += ` (${modifierSigne}${modifier}) = **${result + modifier}**`;
+		}
+		if (action && action.trim()) response += ` : *${action}*`;
+		console.log(action)
+		
+
+		await interaction.editReply({ content: response });
 	},
 };
