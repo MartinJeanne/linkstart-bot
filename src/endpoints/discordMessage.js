ae41671711a3e91e5f6ae1795ed4c87d24fada2f
MartinJeanne <martinjeanne.dev@gmail.com>
2023-09-02 23:50:16 +0200

    fix /playlist

--- src/endpoints/discordMessage.js
+++ src/endpoints/discordMessage.js
@@ -4,7 +4,6 @@ dotenv.config();
 
 const discordMessagesUrl = `${process.env.API_URL}/discordMessages`;
 
-
 module.exports.getDiscordMessages = async function () {
     try {
         const discordMessages = await axios.get(discordMessagesUrl)
