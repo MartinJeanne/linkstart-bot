8db7363fda0cbb602c026b1fdbe40e8ecc07b811
MartinJeanne <martinjeanne.dev@gmail.com>
2024-02-26 18:58:39 +0100

    merge change

--- src/endpoints/discordUser.js
+++ src/endpoints/discordUser.js
@@ -30,7 +30,7 @@ module.exports.getUserPlaylists = async function (user) {
 };
 
 module.exports.checkForBirthday = async function () {
-    return axios.get(`${usersUrl}/birthday`)
+    return axios.get(`${usersUrl}/checkBirthdayIsTomorrow`)
         .then(response => {
             if (response.status === 200) return response.data;
         })
