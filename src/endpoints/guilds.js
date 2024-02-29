726fdc0d1a141c291b2ba019fcae92a297fc4dff
MartinJeanne <martinjeanne.dev@gmail.com>
2024-03-01 00:39:14 +0100

    format

--- src/endpoints/guilds.js
+++ src/endpoints/guilds.js
@@ -7,7 +7,8 @@ const guildsUrl = `${process.env.API_URL}/guilds`;
 module.exports.getGuilds = async function () {
     return axios.get(guildsUrl)
         .then(response => {
-            if (response.status === 200) return response.data;
+            if (response.status === 200) 
+                return response.data;
         })
         .catch(console.error);
 };
@@ -15,17 +16,19 @@ module.exports.getGuilds = async function () {
 module.exports.getGuild = async function (id) {
     return axios.get(`${guildsUrl}/${id}`)
         .then(response => {
-            if (response.status === 200) return response.data;
+            if (response.status === 200)
+                return response.data;
         })
         .catch(console.error);
 };
 
-module.exports.postGuilds = async function (guild) {
+module.exports.postGuild = async function (guild) {
     const newGuild = { id: guild.id, name: guild.name };
 
     return axios.post(guildsUrl, newGuild)
         .then(response => {
-            if (response.status === 201) return response.data;
+            if (response.status === 201)
+                return response.data;
         })
         .catch(console.error);
 };
--- src/endpoints/members.js
+++ src/endpoints/members.js
@@ -1,6 +1,6 @@
 const { default: axios } = require('axios');
 const dotenv = require('dotenv');
-const { getGuild, postGuilds } = require('../endpoints/guilds.js');
+const { getGuild, postGuild } = require('../endpoints/guilds.js');
 dotenv.config();
 
 const membersUrl = `${process.env.API_URL}/members`;
@@ -8,7 +8,9 @@ const membersUrl = `${process.env.API_URL}/members`;
 exports.getMember = async function (member) {
     return axios.get(`${membersUrl}/${member.id}`)
         .then(async response => {
-            if (response.status === 200 && response.data) return response.data;
+            if (response.status === 200 && response.data)
+                return response.data;
+
             else return await exports.postMember(member);
         })
         .catch(console.error);
@@ -17,7 +19,7 @@ exports.getMember = async function (member) {
 exports.postMember = async function (member) {
     await getGuild(member.guild.id)
         .then(async guild => {
-            if (!guild) await postGuilds(member.guild);
+            if (!guild) await postGuild(member.guild);
         })
         .catch(console.error);
 
@@ -30,7 +32,8 @@ exports.postMember = async function (member) {
 
     return axios.post(membersUrl, newUser)
         .then(response => {
-            if (response.status === 201 && response.data) return response.data;
+            if (response.status === 201 && response.data)
+                return response.data;
         })
         .catch(console.error);
 }
@@ -38,7 +41,8 @@ exports.postMember = async function (member) {
 exports.getUserPlaylists = async function (user) {
     return axios.get(`${membersUrl}/${user.id}/playlists`)
         .then(response => {
-            if (response.status === 200) return response.data;
+            if (response.status === 200)
+                return response.data;
         })
         .catch(error => console.error(error));
 };
@@ -46,7 +50,8 @@ exports.getUserPlaylists = async function (user) {
 exports.checkForBirthday = async function () {
     return axios.get(`${membersUrl}/checkBirthdayIsToday`)
         .then(response => {
-            if (response.status === 200) return response.data;
+            if (response.status === 200)
+                return response.data;
         })
         .catch(error => console.error(error));
 };
