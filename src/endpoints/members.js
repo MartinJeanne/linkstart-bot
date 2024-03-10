1fb7e5715c730b0bcffce0d59313e43eadd79295
MartinJeanne <martinjeanne.dev@gmail.com>
2024-03-10 14:26:14 +0100

    rename api-tools to fetch-tools

--- src/endpoints/members.js
+++ src/endpoints/members.js
@@ -1,4 +1,4 @@
-const { members, get, post, put, patch } = require('../functions/api-tools.js');
+const { members, get, post, put, patch } = require('../functions/fetch-tools.js');
 const { getOrCreateGuild } = require('./guilds.js');
 
 exports.getOrCreateMember = async function (member) {
