fed83eaca14ce3e0022acb965c395cc8bcb0b5a6
MartinJeanne <martinjeanne.dev@gmail.com>
2025-03-20 11:32:40 +0100

    unsing try catch in playSavedMusic collector to catch errors here

--- src/service/joue/playSavedMusic.ts
+++ src/service/joue/playSavedMusic.ts
@@ -6,11 +6,11 @@ import savedMusicsEmbedBuilder from './savedMusicsEmbedBuilder';
 import { addSongToQueue } from '../queue/addSongsToQueue';
 import { NoChannelError } from '../../error/generalError/ChannelError';
 import { NoData } from '../../error/botMisuseError/NoData';
+import BotMisuseError from '../../error/botMisuseError/BotMisuseError';
 
 
 export default async function (interaction: ChatInputCommandInteraction): Promise<EmbedBuilder> {
     const player = useMainPlayer();
-    const queue = await getQueue(interaction);
 
     let page = 0;
     const files = fs.readdirSync(`music-files`).filter(file => file.endsWith('.mp3'));
@@ -24,21 +24,26 @@ export default async function (interaction: ChatInputCommandInteraction): Promis
     const collector = interaction.channel.createMessageCollector({ filter: collectorFilter, time: 30_000 });
 
     collector.on('collect', async m => {
-        const i = Number(m.content) - 1; // Song index
-        if (!i || i < 0 || i >= files.length)
-            return await m.reply(':interrobang: identifiant invalide');
-
-        const result = await player.search(`music-files/${files[i]}`, {
-            requestedBy: m.author,
-            searchEngine: QueryType.FILE,
-        });
-
         try {
+            const i = Number(m.content) - 1; // Song index
+            if (!i || i < 0 || i >= files.length)
+                return await m.reply(':interrobang: identifiant invalide');
+
+            const result = await player.search(`music-files/${files[i]}`, {
+                requestedBy: m.author,
+                searchEngine: QueryType.FILE,
+            });
+
+            const queue = await getQueue(interaction);
             const reply = addSongToQueue(result.tracks[0], queue);
             await m.reply(reply);
         } catch (error) {
-            console.error(error);
-            await m.reply('❌ Erreur lors de la lecture de la musique');
+            if (error instanceof BotMisuseError) {
+                await m.reply(error.message);
+            } else {
+                console.error(error);
+                await m.reply('❌ Erreur lors de la lecture de la musique');
+            }
         }
     });
 
